b24ce740531594e1037bb900d66e71794848965f
Sungho Park <chywoo.park@samsung.com>
2015-01-30 16:15:37 +0900

    Modified retreive method structure. retrieve(self, resource_url) is a main method. Other retrieve methods call the mail method.

--- test/jira-test.py
+++ test/jira-test.py
@@ -2,20 +2,22 @@ __author__ = 'chywoo.park'
 import jira
 
 TEST_BASE_URL = "http://jira.score"
-factory = jira.JIRAFactory(2)
+factory = jira.JIRAFactory()
+
 
 def test_JIRAIssue():
     issue = factory.createIssue(TEST_BASE_URL, 'chywoo.park', 'tizensdk*10')
-    issue.retrieve("TS-17952")
-
+    issue.retrieve_issue("TS-17952")
 
     print("Key: " + issue.key)
     print "Key2: ", issue.value("fields/comment/comments/0/updateAuthor/displayName")
+
 
 def test_JIRAIssueType():
     issue = factory.createIssue(TEST_BASE_URL, 'chywoo.park', 'tizensdk*10')
     issue.retrieve_issue_types()
     print "Issue type: ", issue.value()
+
 
 def test_MultiUse():
     SPIN_SERVER_BASE_URL = "http://localhost:2990/jira"
@@ -31,6 +33,6 @@ def test_MultiUse():
     print "SPIN  STATUS : ", status
 
 
-# test_JIRAIssue()
-# test_JIRAIssueType()
+test_JIRAIssue()
+test_JIRAIssueType()
 test_MultiUse()
