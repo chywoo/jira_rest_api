479d5458480d1506feafba2f02eb8b5cb9dafba9
Sungho Park <chywoo.park@samsung.com>
2015-01-30 13:47:12 +0900

    Add 'log' method for logging. You can just self.log(...) method for logging.
    You can set log level in JIRAFactory.createXXX method.

--- rest/jira.py
+++ rest/jira.py
@@ -11,7 +11,7 @@ from restful_lib import Connection
 
 REST_API_URL_POSTFIX = "/rest/api/latest"
 
-jira_debug_level = 1
+jira_debug_level = 0
 
 
 class JIRACommon:
@@ -23,11 +23,26 @@ class JIRACommon:
     body = {}
     httpHeaders = {'Content-type': 'application/json', 'Accept': 'application/json'}
 
+    def log(self, *args):
+        """
+        Debug log if jira_debug_level > 0
+        :param args:
+        :return:
+        """
+        if self.jira_debug_level > 0:
+            print "[REST DEBUG]",
 
-    def __init__(self, base_url, id, password):
+            for message in args:
+                print message,
+
+            print
+
+    def __init__(self, base_url, id, password, debug_level):
         assert (base_url and id and password and base_url != "" and id != "" and password != "")
         assert (isinstance(id, str) and isinstance(password, str))
         global conn
+
+        self.jira_debug_level = debug_level
 
         # Make HTTP authorization key. Connection class has ID and password params but doesn't work.
         self.httpHeaders["Authorization"] = "Basic " + base64.b64encode(id.strip() + ":" + password.strip())
@@ -51,8 +66,10 @@ class JIRACommon:
 
         res = conn.request(self.rest_url, headers=self.httpHeaders, args={})
 
+        self.log("HTTP Response status : " + res[u'headers']['status'])
+
         if res[u'headers']['status'] != "200":
-            print("STATUS == " + res[u'headers']['status'])
+            return None
 
         # Clear value of rest_url for reuse.
         self.rest_url = None
@@ -93,26 +110,24 @@ class JIRAIssue(JIRACommon):
         self.setRESTURL(self.RESOURCE_BASE_URL + issue_key)
         self.request()
 
-        if jira_debug_level > 0:
-            print "[REST DEBUG] Retrived issue data: ", self.body
+        self.log("Retrived issue data: ", self.body)
 
     @property
     def key(self):
         return self.value('key')
 
-    def create(self, project_id, summary, issuetype, assignee = None, priority = None, description = None ):
+    def create(self, project_id, summary, issuetype, assignee=None, priority=None, description=None ):
         pass
 
     def retrieve_issue_types(self):
         self.setRESTURL("/issuetype")
         self.request()
 
-        if jira_debug_level > 0:
-            print "[REST DEBUG] Retrived issue types", self.body
+        self.log("Retrived issue types", self.body)
 
 
 class JIRAFactory:
-    def createIssue(self, url, id, password):
-        return JIRAIssue(url, id, password)
+    def createIssue(self, url, id, password, debug_level=0):
+        return JIRAIssue(url, id, password, debug_level)
 
 
