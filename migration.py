9b46334627af250d6913e59a7aee3ce597a1c068
Sungho Park <chywoo.park@samsung.com>
2015-02-02 18:53:11 +0900

    Changed Labels(SPIN -> SRC,  SCORE -> DST) Add logic which inserts issues  from source jira to destination jira. But, it does not work well. Only some of all issues are inserted.

--- migration.py
+++ migration.py
@@ -2,25 +2,31 @@ __author__ = 'chywoo.park'
 
 import jira
 import util
+import time
 
-SPIN_SERVER_BASE_URL = "http://172.21.17.95:8080"
-# SPIN_SERVER_BASE_URL = "http://168.219.209.56/jira"
-SCORE_SERVER_BASE_URL = "http://localhost:2990/jira"
+SRC_SERVER_BASE_URL = "http://172.21.17.95:8080"
+DST_SERVER_BASE_URL = "http://172.21.17.95:2990/jira"
 
 factory = jira.JIRAFactory()
 
-spin_issue = factory.createIssue(SPIN_SERVER_BASE_URL, 'chywoo.park', 'chywoo.park')
+source_issue = factory.createIssue(SRC_SERVER_BASE_URL, 'chywoo.park', 'chywoo.park')
+dest_issue = factory.createIssue(DST_SERVER_BASE_URL, 'admin', 'admin')
 startAt = 0
 total_count = 0
 
 print ("Key\t\t\tIssueType\tSummary")
 
+DATA_MAP_TO_DEST = {
+    "project":"TEST"
+
+}
+
 loop = True
 while loop:
     print("="*30)
-    spin_issue.retrieve_search("project=TS&maxResults=100&startAt=" + str(startAt))
+    source_issue.retrieve_search("project=TS&maxResults=100&startAt=" + str(startAt))
 
-    data = util.VersatileDict(spin_issue.value())
+    data = util.VersatileDict(source_issue.value())
 
     total_count = int(data.value("total"))
     data.data = data.value("issues")
@@ -30,7 +36,15 @@ while loop:
             loop = False
             break
 
-        print("%d:%d  %s\t%-8s\t%s" % (i + startAt, i, data.value(str(i)+"/key"), data.value(str(i) + "/fields/issuetype/name"), data.value(str(i) + "/fields/summary")))
+        v_key = data.value(str(i)+"/key")
+        v_issuetype = data.value(str(i) + "/fields/issuetype/name")
+        v_summary = data.value(str(i) + "/fields/summary")
+
+        time.sleep(0.1)
+
+        print("%d:%d  %s\t%-8s\t%s" % (i + startAt, i, v_key, v_issuetype, v_summary))
+
+        dest_issue.create_issue(DATA_MAP_TO_DEST['project'], v_summary, v_issuetype)
 
     startAt += 100
 
